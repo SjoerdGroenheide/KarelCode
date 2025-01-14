30f7c4067160cecf592302b2cca713906178ab84
MarijnBergman <OniPyk@gmail.com>
2025-01-14 09:40:27 +0100

    Update Klantenbestand.php

--- Klantenbestand.php
+++ Klantenbestand.php
@@ -146,10 +146,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     <ul class="menu__box">
       <li><a class="menu__item" href="dashboard.php">Home</a></li>
-			<li><a class="menu__item" href="Index.php">About</a></li>
-			<li><a class="menu__item" href="Klantenbestand.php">Team</a></li>
-			<li><a class="menu__item" href="#regrister.php">Contact</a></li>
-			<li><a class="menu__item" href="#">Twitter</a></li>
+			<li><a class="menu__item" href="Klantenbestand.php">Klantenbestand</a></li>
+			<li><a class="menu__item" href="regrister.php">Regrister</a></li>
+			<li><a class="menu__item" href="medewerker.php">Medewerkers</a></li>
     </ul>
   </div>
     <h1>Klantenbeheer</h1>
