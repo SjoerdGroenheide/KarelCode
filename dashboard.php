c02c3e58bb3a2ddc66a9ad271a20d3ecd4aef5b2
MarijnBergman <OniPyk@gmail.com>
2025-01-14 09:48:14 +0100

    ohio

--- dashboard.php
+++ dashboard.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <div class="hamburger-menu">
